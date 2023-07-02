4723a0937d9bf2971d1db89f37b3cf15eff7dc2a
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-02 07:10:25 -0700

    I added IdentityResourceOwnerPasswordValidator

--- SocialNetwork.IdentityServer.Data/ApplicationDbContext.cs
+++ SocialNetwork.IdentityServer.Data/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SocialNetwork.IdentityServer.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SocialNetwork.IdentityServer.Data
 {
@@ -17,9 +14,6 @@ namespace SocialNetwork.IdentityServer.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
