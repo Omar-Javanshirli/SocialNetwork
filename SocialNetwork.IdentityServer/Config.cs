3b61d46fd8d374fffcb866a42d1a4a262e54289a
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-02 03:49:54 -0700

    I did implement identityServer4 framewok in SocialNetwork.IdentityServer

---
+++ SocialNetwork.IdentityServer/Config.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace SocialNetwork.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+            };
+    }
+}
