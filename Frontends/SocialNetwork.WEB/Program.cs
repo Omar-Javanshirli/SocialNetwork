9ee4babd18b2cbdddeff14248acca5c88d19f859
Omar Cavanshirli <86188463+Omar-Javanshirli@users.noreply.github.com>
2023-07-09 03:39:00 +0400

    bug fix

--- Frontends/SocialNetwork.WEB/Program.cs
+++ Frontends/SocialNetwork.WEB/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using SocialNetwork.Web.Core.Models.Settings;
+using SocialNetwork.Web.Core.Services;
+using SocialNetwork.Web.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.Configure<ServiceApiSetting>(builder.Configuration.GetSection("
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddAccessTokenManagement();
 
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 var app = builder.Build();
 
