203e49e51fa212c20b7b780e4cb7b469ccf899bf
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-08-18 16:09:02 -0700

    i did fix error

--- Services/Graph/D. SocialNetwork.Services.Graph.Services/Extensions/DependencyInjection.cs
+++ Services/Graph/D. SocialNetwork.Services.Graph.Services/Extensions/DependencyInjection.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using B._SocialNetwork.Services.Graph.Core.Services;
+using B._SocialNetwork.Services.Graph.Core.UnitOfWorks;
+using C._SocialNetwork.Services.Graph.Repository;
+using D._SocialNetwork.Services.Graph.Service.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace D._SocialNetwork.Services.Graph.Services.Extensions
+public static class DependencyInjection
 {
-    internal class DependencyInjection
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(typeof(D._SocialNetwork.Services.Graph.Services.CQRS.User.Handlers.QueryHandlers.GetAllUserPostsQueryHandler).Assembly);
+        services.AddSingleton<IUnitOfWork, UnitOfWork>();
+        services.AddScoped(typeof(IGenericService<>), typeof(GenericServices<>));
+
+        return services;
     }
 }
+
