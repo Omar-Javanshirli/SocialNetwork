2c675ecaf21895698c46fe1dbde9ec3dada8edda
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-08-25 06:02:49 -0700

    new update

--- Services/Graph/D. SocialNetwork.Services.Graph.Services/CQRS/User/Queries/Response/GetAllUserPostsQueryResponse.cs
+++ Services/Graph/D. SocialNetwork.Services.Graph.Services/CQRS/User/Queries/Response/GetAllUserPostsQueryResponse.cs
@@ -1,8 +1,12 @@
-﻿namespace D._SocialNetwork.Services.Graph.Services.CQRS.User.Queries.Response
+﻿using B._SocialNetwork.Services.Graph.Core.Entities.CommentsEntity;
+using B._SocialNetwork.Services.Graph.Core.Entities.PostsEntity;
+
+namespace D._SocialNetwork.Services.Graph.Services.CQRS.User.Queries.Response
 {
     public class GetAllUserPostsQueryResponse
     {
         public string Id { get; set; } = null!;
         public string UserId { get; set; } = null!;
+        public string Username { get; set; }
     }
 }
