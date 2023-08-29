befa611a2ec9f69e9ffc2d7bd4304325888e9a60
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-08-29 04:45:52 -0700

    new update

--- Services/Graph/D. SocialNetwork.Services.Graph.Services/CQRS/Comment/Handlers/QueryHandlers/GetAllCommentLikeHandler.cs
+++ Services/Graph/D. SocialNetwork.Services.Graph.Services/CQRS/Comment/Handlers/QueryHandlers/GetAllCommentLikeHandler.cs
@@ -1,11 +1,28 @@
-﻿using Azure;
+﻿using AutoMapper;
+using B._SocialNetwork.Services.Graph.Core.UnitOfWorks;
 using D._SocialNetwork.Services.Graph.Services.CQRS.Comment.Quries.Request;
 using D._SocialNetwork.Services.Graph.Services.CQRS.Comment.Quries.Response;
 using MediatR;
+using SocialNetwork.Shared.Dtos;
 
 namespace D._SocialNetwork.Services.Graph.Services.CQRS.Comment.Handlers.QueryHandlers
 {
-    public class GetAllCommentLikeHandler:IRequestHandler<GetAllCommetnLikeRequest,Response<IEnumerable<GetAllCommentLikeResponse>>>
+    public class GetAllCommentLikeHandler : IRequestHandler<GetAllCommentLikeRequest, Response<IEnumerable<GetAllCommentLikeResponse>>>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllCommentLikeHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<IEnumerable<GetAllCommentLikeResponse>>> Handle(GetAllCommentLikeRequest request, CancellationToken cancellationToken)
+        {
+            var commentLikes = await _unitOfWork.commentRepository.GetAllCommentLike(request.CommentId);
+            var response = _mapper.Map<IEnumerable<GetAllCommentLikeResponse>>(commentLikes);
+            return Response<IEnumerable<GetAllCommentLikeResponse>>.Success(response, 200);
+        }
     }
 }
