6fb42c7817fbf97294c70b81f6c68255e68249e6
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-09-01 06:24:20 -0700

    i coded CreateUserMessageCommandConsumer  with MassTransit frameworku ile

--- IdentityServer/SocialNetworkIdentityServer.Service/Services/AuthenticationService.cs
+++ IdentityServer/SocialNetworkIdentityServer.Service/Services/AuthenticationService.cs
@@ -3,17 +3,21 @@ using SocialNetwork.IdentityServer.Core.Models;
 using SocialNetwork.IdentityServer.Core.Models.Input;
 using SocialNetwork.IdentityServer.Core.Services;
 using SocialNetwork.Shared.Dtos;
+using SocialNetwork.Shared.Messages;
+using System;
 using System.Threading.Tasks;
+using Mass = MassTransit;
 
 namespace SocialNetworkIdentityServer.Service.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly UserManager<ApplicationUser> _userManager;
-
-        public AuthenticationService(UserManager<ApplicationUser> userManager)
+        private readonly Mass.IPublishEndpoint _publishEndpoint;
+        public AuthenticationService(UserManager<ApplicationUser> userManager, Mass.IPublishEndpoint publishEndpoint)
         {
             _userManager = userManager;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task<Response<ApplicationUser>> SignUpAsync(SignUpInput input)
@@ -27,6 +31,14 @@ namespace SocialNetworkIdentityServer.Service.Services
 
             if (result is { Succeeded: false })
                 return Response<ApplicationUser>.Fail("Password or Email is wrong", 400);
+
+            await _publishEndpoint.Publish<CreateUserMessageCommand>
+               (new CreateUserMessageCommand
+               {
+                   UserId = Guid.Parse(user.Id),
+                   Email = user.Email,
+                   Username=user.UserName,
+               });
 
             return Response<ApplicationUser>.Success(user, 204);
         }
