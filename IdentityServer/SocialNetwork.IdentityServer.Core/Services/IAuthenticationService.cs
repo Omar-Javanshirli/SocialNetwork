1c513d88d73844ad0e9120994ba2350040a8e782
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-08-31 22:53:46 -0700

    Delete MapProfile class

--- IdentityServer/SocialNetwork.IdentityServer.Core/Services/IAuthenticationService.cs
+++ IdentityServer/SocialNetwork.IdentityServer.Core/Services/IAuthenticationService.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialNetwork.IdentityServer.Core.Models;
+using SocialNetwork.IdentityServer.Core.Models.Input;
+using SocialNetwork.Shared.Dtos;
+using System.Threading.Tasks;
 
 namespace SocialNetwork.IdentityServer.Core.Services
 {
-    internal interface IAuthenticationService
+    public interface IAuthenticationService
     {
+        Task<Response<ApplicationUser>> SignUpAsync(SignUpInput input);
     }
 }
--- IdentityServer/SocialNetworkIdentityServer.Service/Services/AuthenticationService.cs
+++ IdentityServer/SocialNetworkIdentityServer.Service/Services/AuthenticationService.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Identity;
+using SocialNetwork.IdentityServer.Core.Models;
+using SocialNetwork.IdentityServer.Core.Models.Input;
+using SocialNetwork.IdentityServer.Core.Services;
+using SocialNetwork.Shared.Dtos;
+using System.Threading.Tasks;
 
 namespace SocialNetworkIdentityServer.Service.Services
 {
-    internal class AuthenticationService
+    public class AuthenticationService : IAuthenticationService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthenticationService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Response<ApplicationUser>> SignUpAsync(SignUpInput input)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = input.Username,
+                Email = input.Email
+            };
+            var result = await _userManager.CreateAsync(user, input.Password);
+
+            if (result is { Succeeded: false })
+                return Response<ApplicationUser>.Fail("Password or Email is wrong", 400);
+
+            return Response<ApplicationUser>.Success(user, 204);
+        }
     }
 }
