05584ad50fabcb34b00b40caab76b0b3b2e2beb3
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-10-04 03:21:28 -0700

    change identityServer path

--- IdentityServer/SocialNetwork.IdentityServer.Data/ApplicationDbContext.cs
+++ IdentityServer/SocialNetwork.IdentityServer.Data/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SocialNetwork.IdentityServer.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SocialNetwork.IdentityServer.Data
 {
