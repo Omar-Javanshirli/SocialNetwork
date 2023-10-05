4260bea0d1355a9f401b01271f9772a41f46f04d
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-10-04 20:16:40 -0700

    i coded UseExceptionMiddleWare

--- Shared/SocialNetwork.Shared/Middlewares/ExceptionMiddleWare.cs
+++ Shared/SocialNetwork.Shared/Middlewares/ExceptionMiddleWare.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using SocialNetwork.Shared.Dtos;
 
 namespace SocialNetwork.Shared.Middlewares
 {
     public static class ExceptionMiddleWare
     {
-        //public static void UseExceptionMiddleware(this WebApplication app)
-        //{
+        public static void UseExceptionMiddleware(this WebApplication app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
 
-        //}
+                    var response = Response<string>.Fail(exceptionFeature!.Error.Message, 500);
+                    await context.Response.WriteAsJsonAsync(response);
+                });
+            });
+        }
     }
 }
