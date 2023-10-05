2d02d8752836a51440e20a02e1edd93897a2e051
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-10-04 17:41:12 -0700

    i coded Open Telemetry settings

---
+++ Shared/SocialNetwork.OpenTelemetry.Shared/OpenTelemetrySetting/OpenTelemetryConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetwork.OpenTelemetry.Shared.OpenTelemetrySetting
+{
+    public class OpenTelemetryConstants
+    {
+        public string ServiceName { get; set; } = null!;
+        public string ServiceVersion { get; set; } = null!;
+        public string ActivitySourceName { get; set; } = null!;
+    }
+}
